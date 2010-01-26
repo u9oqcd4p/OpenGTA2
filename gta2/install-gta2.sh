8c7e76a6df64a049176bce3ddd6d59b2559850ac
u9oqcd4p <u9oqcd4p@googlemail.com>
2010-01-27 00:39:23 +0100

    Installationsskript in der Basis fertig, Daten wie Checksummen fehlen noch

--- gta2/install-gta2.sh
+++ gta2/install-gta2.sh
@@ -5,13 +5,14 @@
 #
 # Usage: install-gta2.sh <gta2-installation-base>
 #
-
 if [ "" == "${1}" ]; then
 	echo "Usage: install-gta2.sh <gta2-installation base>"
 	exit 1
 fi
 
-# Installationsverzeichnis ueberpruefen
+
+
+# Check installation directory
 if [ -d "${1}" ]; then
 	if [ -e "${1}/gta2.exe" ]; then
 		EXPECTED="7bebb537928c7d870cd4130fd18ea6b4  -"
@@ -28,5 +29,37 @@ else
 	echo "${1} is not a directory"
 	exit 1
 fi
+
+
+
+# Copies file $1 from $2 to ./ and checks if it has the md5sum $3
+function safe_copy {
+	FROM="${2}/${1}"
+	TO="./${1}"
+	if [ -f "${FROM}" ]; then
+		cp "${FROM}" "${1}"
+	else
+		echo "File ${1} doesn't exist in ${2}"
+		return
+	fi
+
+	EXPECTED="${3}  -"
+	RECEIVED=`md5sum < $TO`
+	if [ "${EXPECTED}" != "${RECEIVED}" ]; then
+		echo "Warning: file ${1} is corrupted"
+	fi
+}
+
+
+
+# Copy needed files
+safe_copy "fstyle.sty" "${1}/data" "70f7f4b491a2607e00651cf3a08e378e"
+
+
+
+# Finish work
+echo "All files copied successfully :-)"
+
+
 
 
